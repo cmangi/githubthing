5fa38351f552395d80cf715d9f0bb479e6e3874c
Craig Mangi <cmangi@mmiemail.com>
2013-06-25 09:34:58 -0500

    eh

---
+++ Assets/newScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class newScript : MonoBehaviour {
+	
+	//here is my comment
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
